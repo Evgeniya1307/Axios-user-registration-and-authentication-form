7293e0df60ca824b49d109d2284202f5010329e4
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-08-02 14:44:39 +0400

    //onChange={e=>setLogin}/> приходит событие (е => из е достану значение )

--- src/pages/Main/components/Login/Login.tsx
+++ src/pages/Main/components/Login/Login.tsx
@@ -1,18 +1,31 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent } from "react";
 
+const Login = () => {
+//2 state
+const [login, setLogin]=React.useState('');
+const[password, setPassword]=React.useState('');
 
-const Login =()=>{
-    const handleSubmit = (e:FormEvent) => { // в эту фу-ию будет приходить событие 
-//отменяю событие чтобы вополнился код 
-e.preventDefault();
-    }
-    return(
+  const handleSubmit = (e: FormEvent) => {
+    // в эту фу-ию будет приходить событие
+    //отменяю событие чтобы вополнился код
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <div>
-            <form onSubmit={handleSubmit}>
-
-            </form>
+          <label htmlFor="login">Login:</label>
+          <input name="login" type="text" value={login} onChange={e=>setLogin(e.target.value)}/>
         </div>
-    )
-}
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input name="password" type="password" value={password} onChange={e=>setPassword(e.target.value)}/>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
+
+//onChange={e=>setLogin}/> приходит событие (е => из е достану значение )
