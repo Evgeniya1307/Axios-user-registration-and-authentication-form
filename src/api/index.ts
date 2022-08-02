4e8f0138cef492d8042521ffb8eadfb9af45d206
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-08-02 15:31:12 +0400

    //:AxiosPromise<ILoginResponse> описала что вернёт на выход аксиос промис и вернётILoginResponse

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,9 @@
-import { ILoginRequest } from './auth/types';
+import { ILoginRequest, ILoginResponse } from './auth/types';
 import { axiosInstance } from './auth/instance';
 import Endpoints from './endpoints';
+import { AxiosPromise } from 'axios';
 
 //запрос
-export const login=(params:ILoginRequest)=>axiosInstance.post(Endpoints.AUTH.Login)//создаю запрос пост 1параметр ендпоинтс куда нужно стучаться, 2параметр отдаю параметры
+export const login=(params:ILoginRequest):AxiosPromise<ILoginResponse>=>axiosInstance.post(Endpoints.AUTH.Login)//создаю запрос пост 1параметр ендпоинтс куда нужно стучаться, 2параметр отдаю параметры
+
+//:AxiosPromise<ILoginResponse> описала что вернёт на выход аксиос промис и вернётILoginResponse 
