d2c28025f3d985f6006b8bfa6fdfbe42cf938f17
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-08-03 11:27:00 +0400

    authData: {       accessToken: string | null //храню то  что получила при аторизации

--- src/store/auth/authReducer.ts
+++ src/store/auth/authReducer.ts
@@ -1,19 +1,88 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
-import logger from "redux-logger";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-
-// общие настройки для redux-store
-export const store = configureStore({
-    reducer:{
-        //autch:autchReducer,
+//интерфейс для редюсера для слайса
+export interface AuthState {
+    authData: {
+      accessToken: string | null //храню то  что получила при аторизации 
+      isLoading: boolean
+      error:  string | null,
+    }
+    profileData: {
+      profile: string | null,
+      isLoading: boolean
+      error:  string | null,
+    }
+  }
+  
+  const initialState: AuthState = {
+    authData: {
+      accessToken: null,
+      isLoading: false,
+      error:  null,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(...(process.env.NODE_ENV !== 'production' ? [logger] : [])),
-})
-
-export type IRootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-export const useAppDispatch: () => AppDispatch = useDispatch // Export a hook that can be reused to resolve types
-
-
-//configureStore с помощью этой фу-ии создаю store
+    profileData: {
+      profile: null,
+      isLoading: false,
+      error:  null,
+    }
+  }
+  
+  export const authReducer = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+      loginStart: (state): AuthState => ({
+        ...state,
+        authData: {
+          ...state.authData,
+          isLoading: true,
+        }
+      }),
+      loginSucess: (state, action: PayloadAction<string>): AuthState => ({
+        ...state,
+        authData: {
+          ...state.authData,
+          accessToken: action.payload,
+          isLoading: false,
+          error:  null,
+        }
+      }),
+      loginFailure: (state, action: PayloadAction<string>): AuthState => ({
+        ...state,
+        authData: {
+          ...state.authData,
+          isLoading: false,
+          error:  action.payload,
+        }
+      }),
+      loadProfileStart: (state): AuthState => ({
+        ...state,
+        profileData: {
+          ...state.profileData,
+          isLoading: true,
+        }
+      }),
+      loadProfileSucess: (state, action: PayloadAction<string>): AuthState => ({
+        ...state,
+        profileData: {
+          ...state.profileData,
+          profile: action.payload,
+          isLoading: false,
+          error:  null,
+        }
+      }),
+      loadProfileFailure: (state, action: PayloadAction<string>): AuthState => ({
+        ...state,
+        profileData: {
+          ...state.profileData,
+          isLoading: false,
+          error:  action.payload,
+        }
+      }),
+      logoutSuccess: (): AuthState => initialState,
+    },
+  })
+  
+  export const { loadProfileStart, loadProfileSucess, loadProfileFailure, loginStart, loginSucess, loginFailure, logoutSuccess } = authReducer.actions
+  
+  export default authReducer.reducer
---
+++ src/store/index.ts
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import logger from "redux-logger";
+
+
+// общие настройки для redux-store
+export const store = configureStore({
+    //мои созданные редюсеры
+    reducer:{
+        //autch:autchReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(...(process.env.NODE_ENV !== 'production' ? [logger] : [])),
+})
+
+//описала для ts
+export type IRootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch // Export a hook that can be reused to resolve types
+
+
+//configureStore с помощью этой фу-ии создаю store
