f355e3120601bbfe3a952e5869f1402403a8ea0d
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-08-04 12:49:15 +0400

    baseURL: 'http://localhost:3500' //базовый url юэкенд из node.js будет на порту 3500

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({ //позволяет ус-ть базовый url адрес для полного приложения 
-    baseURL: 'http://localhost:3500' //базовый url 
+    baseURL: 'http://localhost:3500' //базовый url юэкенд из node.js будет на порту 3500
 });
